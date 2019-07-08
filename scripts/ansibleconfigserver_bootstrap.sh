be876dc1e7384577c7156b4a81676da6548ba06f
DAL PAN <olivier.dal-pan@sogeti.com>
2019-07-08 15:51:37 +0200

    Added : cluster-admin rights for admin user.

--- scripts/ansibleconfigserver_bootstrap.sh
+++ scripts/ansibleconfigserver_bootstrap.sh
@@ -207,7 +207,7 @@ AWSSB_SETUP_HOST=$(head -n 1 /tmp/openshift_initial_masters)
 set +x
 OCP_PASS=$(aws secretsmanager get-secret-value --secret-id  ${OCP_PASS_ARN} --region ${AWS_REGION} --query SecretString --output text)
 ansible masters -a "htpasswd -b /etc/origin/master/htpasswd admin ${OCP_PASS}"
-#ansible masters -a "oc adm policy add-cluster-role-to-user cluster-admin admin"
+ansible masters -a "oc adm policy add-cluster-role-to-user cluster-admin admin"
 set -x
 
 mkdir -p ~/.kube/
